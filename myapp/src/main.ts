8cc599c899addd765a40c00de7d9acade71bc676
zhangjinhui <344879340@qq.com>
2019-07-17 15:45:55 +0800

    更改main

--- myapp/src/main.ts
+++ myapp/src/main.ts
@@ -4,10 +4,25 @@ import router from "./router";
 import store from "./store";
 import "./registerServiceWorker";
 
+import './assets/css/reset.css'
+import './assets/css/common.css'
+import '../node_modules/github-markdown-css/github-markdown.css'
+import 'iview/dist/styles/iview.css'
+import '../node_modules/font-awesome/css/font-awesome.css'
+import '../node_modules/simplemde/debug/simplemde.css'
+
+import * as filters from './util/filter'
+import iView from 'iview'
+
 Vue.config.productionTip = false;
+Vue.use(iView)
+
+Object.keys(filters).forEach(k => Vue.filter(k, filters[k])) //注册过滤器
 
 new Vue({
   router,
   store,
   render: h => h(App)
 }).$mount("#app");
+
+
